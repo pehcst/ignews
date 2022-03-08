30bc144c7ea8f26841fbfa43d64605a062a6ef77
Pedro Costa <pehcst@outlook.com.br>
2022-03-08 16:15:00 -0300

    chore: auth and db connection

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -9,4 +12,38 @@ export default NextAuth({
       authorization: { params: { scope: "read:user" } },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      console.log(email);
+
+      try {
+        fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("users_by_email"),
+                  q.Casefold(user.email as string)
+                )
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(
+              q.Exists(
+                q.Match(
+                  q.Index("users_by_email"),
+                  q.Casefold(user.email as string)
+                )
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
